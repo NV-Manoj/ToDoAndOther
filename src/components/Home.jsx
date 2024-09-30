64221b4f31472fd833884933a9aa13793d266481
Manoj <nvmanoj21@gmail.com>
2024-09-30 14:20:39 +0530

    Changed The Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import homeIMG from "../logo192.png";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <motion.img animate={{rotate:360}} transition={{repeat:Infinity , duration:20}} className="h-full w-full object-contain" src={homeIMG} alt="MyImg" />
+      <motion.img
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 20 }}
+        className="h-full w-full object-contain"
+        src={homeIMG}
+        alt="MyImg"
+      />
     </div>
   );
 };
