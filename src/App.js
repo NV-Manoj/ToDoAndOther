bc256042c3b1f0c9bb1fea980b68a03e5c28108b
Manoj <nvmanoj21@gmail.com>
2024-09-30 11:44:37 +0530

    Integrated Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import NavbarComponent from "./components/Navbar";
 import Cards from "./components/cards/Cards";
-import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Todo />} />
+        <Route path="/" element={<NavbarComponent />} />
+        <Route path="/counter" element = {<Counter/>} />
+        <Route path="/toggle" element={<ToggleTheme />} />
+        <Route path="/basicexample" element={<BasicExample />} />
+        <Route path="/toggle" element={<ToggleTheme />} />
         <Route path="/toggle" element={<ToggleTheme />} />
       </Routes>
     </div>
   );
 }
-
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,9 @@ import Navbar from "react-bootstrap/Navbar";
 function NavbarComponent() {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Container>
+      <div>
+        <h2>My Applications</h2>
+      </div>
     </Navbar>
   );
 }
